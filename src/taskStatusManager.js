0e4c58288cff875aa4d1555ccae0198d4d9d5c49
Debabrata Mandal <bapidebabrata@gmail.com>
2022-06-01 22:29:26 +0530

    Fix linter error

--- src/taskStatusManager.js
+++ src/taskStatusManager.js
@@ -6,7 +6,7 @@ export default function updateListItem(status, index) {
 
 document
   .getElementById('clear-completed')
-  .addEventListener('click', (event) => {
+  .addEventListener('click', () => {
     const elements = document.getElementsByClassName('task-status');
     for (let i = elements.length - 1; i >= 0; i -= 1) {
       if (elements[i].checked) elements[i].parentElement.remove();
