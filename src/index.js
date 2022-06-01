8913e0b62343c56744b5b8d2c81ab5ed5e27badb
Debabrata Mandal <bapidebabrata@gmail.com>
2022-06-01 19:51:24 +0530

    Add javascript to add, remove and update tasks

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 // import _ from 'lodash';
 import './reset.css';
 import './style.css';
+import './taskmanager.js';
 import dragDropIcon from './images/drag-drop-icon.svg';
+import deleteIcon from './images/delete-icon.svg';
+import TaskManager from './taskmanager.js'
 
-const toDoList = [
-  {
-    description: 'Demo Task1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Demo Task1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Demo Task1',
-    completed: false,
-    index: 2,
-  },
-];
+let toDoList = new TaskManager('todoList');
 
 function getListItem(task) {
   const todoTaskElement = document.createElement('li');
+  let focusedOnElement = false;
   todoTaskElement.classList.add('todo-list-item');
   {
     const todoTaskStatus = document.createElement('input');
@@ -32,23 +20,73 @@ function getListItem(task) {
     todoTaskElement.appendChild(todoTaskStatus);
   }
   {
-    const todoTaskDescription = document.createElement('p');
-    todoTaskDescription.textContent = task.description;
+    const todoTaskDescription = document.createElement('input');
+    todoTaskDescription.value = task.description;
     todoTaskDescription.classList.add('task-description');
+
+    todoTaskDescription.addEventListener('keyup', event => {
+      toDoList.updateDescription(event.target.value, task.index);
+    })
+
+    todoTaskDescription.addEventListener('focus', event => {
+      focusedOnElement = true;
+      event.target.nextSibling.src = deleteIcon;
+      event.target.nextSibling.alt = 'delete';
+      event.target.nextSibling.className = 'delete-icon';
+      event.target.style.backgroundColor = '#fffeca'
+      event.target.parentElement.style.backgroundColor = '#fffeca'
+    })
+
+    todoTaskDescription.addEventListener('blur', event => {
+      focusedOnElement = false;
+      event.target.nextSibling.src = dragDropIcon;
+      event.target.nextSibling.alt = 'drag drop';
+      event.target.nextSibling.className = 'task-drag-icon';
+      event.target.style.backgroundColor = ''
+      event.target.parentElement.style.backgroundColor = ''
+    })
+
     todoTaskElement.appendChild(todoTaskDescription);
   }
   {
     const todoTaskDragIcon = document.createElement('img');
     todoTaskDragIcon.src = dragDropIcon;
     todoTaskDragIcon.alt = 'drag drop';
-    todoTaskDragIcon.classList.add('task-drag-icon');
+    todoTaskDragIcon.className = 'task-drag-icon';
+    todoTaskDragIcon.addEventListener('mousedown', () => {
+      if (focusedOnElement) {
+        todoTaskElement.remove();
+        toDoList.remove(task.index);
+      }
+    })
     todoTaskElement.appendChild(todoTaskDragIcon);
   }
   return todoTaskElement;
 }
 
 function updateToDoListToHTML() {
-  toDoList.forEach((task) => document.getElementById('todo-list').appendChild(getListItem(task)));
+  toDoList.taskList.forEach((task) => document.getElementById('todo-list').appendChild(getListItem(task)));
 }
+
+function addTask(description) {
+  if (description)
+  document.getElementById('todo-list').appendChild(getListItem(toDoList.add(description)));
+}
+
+const addItemElement = document.getElementById('add-item')
+
+addItemElement.addEventListener('keypress', event => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addTask(event.target.value);
+    event.target.value = ''
+  }
+})
+
+document.querySelector("#add-item-container > img").addEventListener('click', (event) => {
+  event.preventDefault();
+  addTask(addItemElement.value);
+  addItemElement.value = ''
+})
 
 updateToDoListToHTML();
