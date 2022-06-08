2c677b1821dd20502e01090d2f51ad1fd45a8bcc
jheart-vic <adebowalevictorjheart.com>
2022-06-08 15:28:41 +0100

    Add test for remove function

--- test/taskmanager.test.js
+++ test/taskmanager.test.js
@@ -1,13 +1,20 @@
 import TaskManager from '../src/taskmanager'
 
 describe("Testing add item & remove item", () => {
+    let newList = new TaskManager('tasks');
     test("Add item", ()=> {
-        let newList = new TaskManager('tasks');
         newList.add("New Task");
         expect(newList.taskList.length).toBe(1);
         newList.add("New Task");
         newList.add("New Task");
         expect(newList.taskList.length).toBe(3);
+    }),
+    test("Remove item", ()=> {
+        newList.remove(3);
+        expect(newList.taskList.length).toBe(2);
+        newList.remove(1);
+        newList.remove(1);
+        expect(newList.taskList.length).toBe(0);
     })
 
 })
