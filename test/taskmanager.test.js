945ad2ae454b563310111dbe19c2ddc428cd0a1b
Debabrata Mandal <bapidebabrata@gmail.com>
2022-06-08 20:22:06 +0530

    Fix linters error

--- test/taskmanager.test.js
+++ test/taskmanager.test.js
@@ -1,20 +1,19 @@
-import TaskManager from '../src/taskmanager'
+import TaskManager from '../src/taskmanager.js';
 
-describe("Testing add item & remove item", () => {
-    let newList = new TaskManager('tasks');
-    test("Add item", ()=> {
-        newList.add("New Task");
-        expect(newList.taskList.length).toBe(1);
-        newList.add("New Task");
-        newList.add("New Task");
-        expect(newList.taskList.length).toBe(3);
-    }),
-    test("Remove item", ()=> {
-        newList.remove(3);
-        expect(newList.taskList.length).toBe(2);
-        newList.remove(1);
-        newList.remove(1);
-        expect(newList.taskList.length).toBe(0);
-    })
-
-})
+describe('Testing add item & remove item', () => {
+  const newList = new TaskManager('tasks');
+  test('Add item', () => {
+    newList.add('New Task');
+    expect(newList.taskList.length).toBe(1);
+    newList.add('New Task');
+    newList.add('New Task');
+    expect(newList.taskList.length).toBe(3);
+  });
+  test('Remove item', () => {
+    newList.remove(3);
+    expect(newList.taskList.length).toBe(2);
+    newList.remove(1);
+    newList.remove(1);
+    expect(newList.taskList.length).toBe(0);
+  });
+});
